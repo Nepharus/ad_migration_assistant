111831e7d864a99c4f814414602ecb6705d7b18f
Nepharus <risk3715@gmail.com>
2015-04-15 16:24:46 -0600

    Set ad_migrate.sh with execution bit
    Found way to simplify my IF conditional expression and put it in test.sh

--- test.sh
+++ test.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 # Run a check that the DNS server is reachable
 
-#if ping -c 2 -q ad1.alpine.local &> /dev/null
-#then
-#	echo "There 1"
-#	exit;
-#else
-#	if ping -c 2 -q ad2.alpine.local &> /dev/null
-#	then
-#		echo "There 2"
-#		exit;
-#	else
-#		if ping -c 2 -q ad3.alpine.local &> /dev/null
-#		then
-#			echo "There 3"
-#			exit;
-#		else
-#			echo "Not there"
-#			exit;
-#		fi
-#	fi
-#fi
-#exit
+IPs=( "ad1.alpine.local"  "ad2.alpine.local"  "ad3.alpine.local"  )
 
-cond1=$(ping -c 2 -q ad1.alpine.local.1.111 &> /dev/null)
-cond2=$(ping -c 2 -q ad2.alpine.local.2.111 &> /dev/null)
-
-if [ $cond1 || $cond2 ];
-then
-	echo "One of them is accessible"
-else
-	echo "Neither are accessible"
-fi
+for ip in ${IPs[*]}; do
+    if ping -c 2 -q $ip &> /dev/null ; then
+        echo found $ip up and running. 
+        exit 0
+    fi
+done 
+echo Armaggedon. None of these servers exist anymore.
+echo Try again later after having recreated world.
