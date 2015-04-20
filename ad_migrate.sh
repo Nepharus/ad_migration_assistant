d0b7e5777baf359a5487f3c795022d6fab462dca
Nepharus <risk3715@gmail.com>
2015-04-20 09:11:40 -0600

    Ping set to dump to null

--- ad_migrate.sh
+++ ad_migrate.sh
@@ -66,20 +66,20 @@ check_servers(){
 for ip in ${IPs[*]};
 do
 # If ping returns true, change found_one to true
-	if ping -c 2 -q $ip;
-	then
-		found_one=0
-	fi
-done 
+	if ping -c 2 -q $ip &> /dev/null
+    then
+    found_one=0
+    fi
+done
 return $found_one
 }
 
 # Check the function is not true
 if ! check_servers;
 then
-	echo "None of the DC's are available. Wire the machine on a school network."
-	sudo networksetup -setairportpower en1 on
-	exit
+        echo "None of the DC's are available. Wire the machine on a school network."
+        sudo networksetup -setairportpower en1 on
+        exit
 fi
 
 # Find out who's running the script
--- test.sh
+++ test.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+IPs=( "ad1.alpine.local" "ad2.alpine.local" "ad3.alpine.local" )
+# Value to return false
+found_one=1
 
-# Check that chown will work first
-# Specify test user
-new_user=monkey
-
-# Function for the check
-chown_check(){
-# Run command
-echo "Attempting to set ownership of their home folder"
-echo "This may take a little while- $(tput setaf 2)Please, be patient$(tput sgr0)"
-sudo chown -R $new_user:staff /Users/$new_user &>/dev/null
-# $? is the value of true or false of last command
-chown_is=$?
-ls -al /Users/
-return $chown_is
+# Function for checking for servers
+check_servers(){
+for ip in ${IPs[*]};
+do
+# If ping returns true, change found_one to true
+	if ping -c 2 -q $ip &> /dev/null
+	then
+		found_one=0
+	fi
+done 
+return $found_one
 }
 
-if ! chown_check;
+# Check the function is not true
+if ! check_servers;
 then
-	echo "We can't chown"
+	echo "None of the DC's are available. Wire the machine on a school network."
+	sudo networksetup -setairportpower en1 on
 	exit
 fi
